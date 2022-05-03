abcb4c8ee2fb3ecd990f36eaae0dfc4ed66abab9
lukasz <l.m.ratajczak94@gmail.com>
2022-05-03 18:56:28 +0200

    base templates added

--- base/views.py
+++ base/views.py
@@ -1,8 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "home.html")
+rooms = [
+    {'id': 1, 'name': "Check 1"},
+    {'id': 2, 'name': "Two"},
+    {'id': 3, 'name': "Top 3"}
+]
 
-def room(request):
-    return render(request, 'room.html')
+
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, "base/home.html", context)
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
